f5fe703bec2b5bf30a75ae6210bf8ca2d340ed12
Mahdi Baccara <mahdi.baccara@algowatt.com>
2024-04-16 09:33:39 +0200

    small change

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,17 @@
-import "./registerServiceWorker";
-import "vfonts/Lato.css";
-import "vfonts/FiraCode.css";
-import "@/assets/tailwind.css"; // useless? maybe but it's comfy
-import "@/assets/style/app.css"; // useless? maybe but it's comfy
-
-import App from "./App.vue";
 import { createApp } from "vue";
-import naive from "naive-ui";
-import router from "./router";
+import App from "./App.vue";
+import "./registerServiceWorker";
 
 //optional imports
-
+import naive from "naive-ui";
 // General Font
-
+import "vfonts/Lato.css";
 // Monospace Font
+import "vfonts/FiraCode.css";
 
-
-
-
-
+import "@/assets/tailwind.css"; // useless? maybe but it's comfy
+import "@/assets/style/app.css"; // useless? maybe but it's comfy
+import router from "./router";
 
 const app = createApp(App); /* .mount("#app"); */
 app.use(naive).use(router).mount("#app");
